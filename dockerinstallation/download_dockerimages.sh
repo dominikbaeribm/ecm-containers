90e480922380558ff003ce3d3b1b6e7c70e61d0c
dominikbaer1 <dominikbaer1@192.168.1.195>
2018-03-02 14:17:39 +0100

    correct errors in downlaod ibm images

--- dockerinstallation/download_dockerimages.sh
+++ dockerinstallation/download_dockerimages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker docker pull ibmcom/db2express-c
+# docker pull ibmcom/db2express-c
 docker login dominikbaeribm
 docker pull ibmcorp/ecm_earlyadopters_cpe:earlyadopters-gm5.5
 docker pull ibmcorp/ecm_earlyadopters_icn:earlyadopters-gm5.5
