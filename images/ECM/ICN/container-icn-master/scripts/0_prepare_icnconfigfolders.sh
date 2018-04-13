4973812e5a789970555da1911b488ac48650b2eb
dominikbaer1 <dominikbaer1@10.192.124.58>
2018-04-13 10:55:59 +0200

    updated icn installation

---
+++ images/ECM/ICN/container-icn-master/scripts/0_prepare_icnconfigfolders.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+mkdir -p /home/ecm_ad_db2/icn/configDropins/overrides
+mkdir -p /home/ecm_ad_db2/icn/logs
+mkdir -p /home/ecm_ad_db2/icn/plugins
+mkdir -p /home/ecm_ad_db2/icn/viewerlog 
+mkdir -p /home/ecm_ad_db2/icn/viewercache
+chown -R 501:500 /home/ecm_ad_db2/icn/viewerlog
+chown -R 501:500 /home/ecm_ad_db2/icn/viewercache
+chown -R 501:500 /home/ecm_ad_db2/icn/plugins
+chown -R 501:500 /home/ecm_ad_db2/icn/logs
+chown -R 501:500 /home/ecm_ad_db2/icn/configDropins/overrides
+
+cd /home/ecm_ad_db2/icn
+
+cp /mnt/hgfs/ecm-containers/images/ECM/ICN/container-icn-master/examples/ICNDS.xml .
+cp /mnt/hgfs/ecm-containers/images/ECM/CPE/container-cpe-master/examples/ldapAD.xml .
+cp /mnt/hgfs/ecm-containers/images/ECM/CPE/container-cpe-master/examples/DB2JCCDriver.xml .
+cp /mnt/hgfs/ecm-containers/images/ECM/CPE/container-cpe-master/examples/db2jcc_license_cu.jar .
+cp /mnt/hgfs/ecm-containers/images/ECM/CPE/container-cpe-master/examples/db2jcc4.jar .
